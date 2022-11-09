e4211b71e80bffab37c1d34d314d77ca0eca8f7b
Robert Low <robert.low91@gmail.com>
2022-11-09 17:21:59 +0000

    examples

--- spec/before_and_after_spec.rb
+++ spec/before_and_after_spec.rb
@@ -3,6 +3,18 @@ RSpec.describe 'before and after hooks' do
     puts 'I run before the main describe block above.'
   end
 
+  after(:context) do
+    puts 'I run after the describe block above.'
+  end
+
+  before(:example) do
+    puts 'I run before each example (so twice in this case)'
+  end
+
+  after(:example) do
+    puts 'I run after each example (so twice in this case)'
+  end
+
   it 'is just a random example' do
     expect(5 * 4).to eq(20)
   end
