fec77544e10e5f1ac0b2ed6cf18a5e36ad2eff09
Robert Low <robert.low91@gmail.com>
2022-11-09 17:16:24 +0000

    before context block

--- spec/before_and_after_spec.rb
+++ spec/before_and_after_spec.rb
@@ -1,4 +1,8 @@
 RSpec.describe 'before and after hooks' do
+  before(:context) do
+    puts 'I run before the main describe block above.'
+  end
+
   it 'is just a random example' do
     expect(5 * 4).to eq(20)
   end
@@ -7,6 +11,3 @@ RSpec.describe 'before and after hooks' do
     expect(4 - 3).to eq(1)
   end
 end
-
-
-wfwfwf
