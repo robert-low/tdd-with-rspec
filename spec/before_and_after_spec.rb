0bb6ad9a1a7df7d59ed6065a470d865b76bcfa4d
Robert Low <robert.low91@gmail.com>
2022-11-11 14:36:18 +0000

    comments

--- spec/before_and_after_spec.rb
+++ spec/before_and_after_spec.rb
@@ -23,3 +23,13 @@ RSpec.describe 'before and after hooks' do
     expect(4 - 3).to eq(1)
   end
 end
+
+#hook 1
+# before(:example) - runs before every example/test
+# after(:example) - runs after every example
+# good for speed - no need to instantiate a brand new object - option is available
+
+#hook 2
+#before(:context) - runs before whereever it is internally inside/overall
+#after(:context) - runs after whereever it is internally inside
+# the current block that that code is written in - NOT in between tests
