726f8184ebd2b6ead59edbcae2edb08d52854e72
Robert Low <robert.low91@gmail.com>
2022-11-11 16:01:53 +0000

    class and tests added

--- spec/overwriting_let_spec.rb
+++ spec/overwriting_let_spec.rb
@@ -4,3 +4,21 @@ class ProgrammingLanaguage
     @name = name
   end
 end
+
+RSpec.describe ProgrammingLanaguage do
+  let(:language) { ProgrammingLanaguage.new('Python')}
+  # every child has access to the parent helper method (e.g let)
+
+  it 'should store the name of the language' do
+    expect(language.name).to eq('Python')
+  end
+
+  context 'with no initialisation argument' do
+    let(:langugae) { ProgrammingLanaguage.new }
+    #however nested variables overwrite parent variables
+
+    it 'should default to "Ruby" as the name' do
+      expect(language.name).to eq('Ruby')
+    end
+  end
+end
