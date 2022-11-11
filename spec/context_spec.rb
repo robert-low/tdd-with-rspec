25cb228f06e23b6a8188bad4a18a73f70ccce806
Robert Low <robert.low91@gmail.com>
2022-11-11 14:26:10 +0000

    comments

--- spec/context_spec.rb
+++ spec/context_spec.rb
@@ -1,6 +1,12 @@
 # nested describe methods with context, for when your tests
 # have conditions.
 
+# you can nest describe/context blocks within other describe/blocks
+
+# conditionals are a sign you should extract it to a higher level of
+# describe block.
+
+# context is an alias for describe
 
 RSpec.describe '#even method' do
   context 'with even number' do
