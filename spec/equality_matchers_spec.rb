5ae1b60dfc52d0b9c40759108e35f5297448557a
Robert Low <robert.low91@gmail.com>
2022-11-18 14:54:18 +0000

    equality matcher eq and eql

---
+++ spec/equality_matchers_spec.rb
@@ -0,0 +1,23 @@
+RSpec.describe 'equality matchers' do
+  let(:a) { 3.0 }
+  let(:b) { 3 }
+
+  describe 'eq matcher' do
+    it 'tests for value and ignores type' do
+      expect(a).to eq(3)
+      expect(b).to eq(3.0)
+      expect(a).to eq(b)
+    end
+  end
+
+  describe 'eql matcher' do
+    it 'tests for value, including type' do
+      expect(a).not_to eql(3)
+      expect(b).not_to eql(3.0)
+      expect(a).not_to eql(b)
+
+      expect(a).to eql(3.0)
+      expect(b).to eql(3)
+    end
+  end
+end
