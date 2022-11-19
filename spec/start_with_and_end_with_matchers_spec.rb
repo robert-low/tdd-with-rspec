2461ada11cb31c410d2b04c3c14a7e26cf15179f
Robert Low <robert.low91@gmail.com>
2022-11-19 12:48:02 +0000

    shorthand syntax

--- spec/start_with_and_end_with_matchers_spec.rb
+++ spec/start_with_and_end_with_matchers_spec.rb
@@ -10,4 +10,14 @@ RSpec.describe 'start with and end with matchers' do
     it { is_expected.to start_with('cat') }
     it { is_expected.to end_with('pillar') }
   end
+
+  describe [:a, :b, :c, :d] do
+    it 'should check for elements at the beginning or end of the array' do
+      expect(subject).to start_with(:a)
+      expect(subject).to start_with(:a, :b)
+      expect(subject).to end_with(:c, :d)
+    end
+    it { is_expected.to end_with(:d) }
+    it { is_expected.to start_with(:a) }
+  end
 end
