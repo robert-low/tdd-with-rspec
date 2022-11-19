df94e8e946da34e7d2d04b2fabb9f2c7b828e348
Robert Low <robert.low91@gmail.com>
2022-11-19 12:17:53 +0000

    negative change method invoked

--- spec/change_matcher_spec.rb
+++ spec/change_matcher_spec.rb
@@ -4,3 +4,17 @@ RSpec.describe 'change matchers' do
     expect { subject.push(4) }.to change { subject.length }.from(3).to(4)
   end
 end
+
+# however this method is too closely connected to the subject.
+# better to write it like this: 'by' method
+
+RSpec.describe 'change matchers' do
+  subject {[1, 2, 3]}
+  it 'checks that a method changes object state' do
+    expect { subject.push(4) }.to change { subject.length }.by(1)
+  end
+
+  it 'accepts negative arguments' do
+    expect { subject.pop }.to change { subject.length }.by(-1)
+  end
+end
