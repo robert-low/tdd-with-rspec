8f58880a52cfd980b1b5a29358c73f832c4b88cb
Robert Low <robert.low91@gmail.com>
2022-11-22 12:11:46 +0000

    re-written test with receive_messages method

--- spec/double_spec.rb
+++ spec/double_spec.rb
@@ -17,3 +17,15 @@ RSpec.describe 'a random double' do
     expect(stuntman.fall_off_ladder).to eq('Ouch')
   end
 end
+
+# can also be written with receive_messages - can pass an argument:
+
+RSpec.describe 'a random double' do
+  it 'only allows defined methods to be invoked' do
+    stuntman = double("Mr Danger")
+    allow(stuntman).to receive_messages(fall_off_ladder: 'Ouch',
+    light_on_fire: true)
+    expect(stuntman.light_on_fire).to eq(true)
+    expect(stuntman.fall_off_ladder).to eq('Ouch')
+  end
+end
