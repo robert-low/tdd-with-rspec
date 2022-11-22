88430af1e92581a56bd8e2222cc3a025a1264d83
Robert Low <robert.low91@gmail.com>
2022-11-22 12:27:52 +0000

    double spec assignment

--- spec/double_spec_assignment.rb
+++ spec/double_spec_assignment.rb
@@ -4,9 +4,6 @@
 # The double's methods should be assigned in the initial invocation of the double method.
 # Write two expectations, one for connect and one for disconnect, that confirms the return value of each.
 # Assign the double to the variable db.
-# Create a double with the name "File System". Assign the double to the variable fs.
-# Using the allow method, give the double a read method that returns the value "Romeo and Juliet".
-# Using the allow method, give the double a write method that returns the value false.
 
 
 RSpec.describe 'doubles' do
@@ -15,5 +12,34 @@ RSpec.describe 'doubles' do
     db = double("Database Connection", connect: true, disconnect: 'Goodbye')
     expect(db.connect).to eq(true)
     expect(db.disconnect).to eq('Goodbye')
+  end
+end
+
+# Create a double with the name "File System". Assign the double to the variable fs.
+# # Using the allow method, give the double a read method that returns the value "Romeo and Juliet".
+# # Using the allow method, give the double a write method that returns the value false.
+
+RSpec.describe 'doubles' do
+  it "tests the student's knowedge of the course's content" do
+    fs = double("File System")
+    allow(fs).to receive(:read).and_return("Romeo and Juliet")
+    allow(fs).to receive(:write).and_return(false)
+
+    expect(fs.read).to eq("Romeo and Juliet")
+    expect(fs.write).to eq(false)
+  end
+end
+
+#  or:
+
+RSpec.describe 'doubles' do
+  it "tests the student's knowledge of the course's content" do
+
+    fs = double("File System")
+
+    allow(fs).to receive_messages(read: "Romeo and Juliet", write: false)
+
+    expect(fs.read).to eq("Romeo and Juliet")
+    expect(fs.write).to eq(false)
   end
 end
