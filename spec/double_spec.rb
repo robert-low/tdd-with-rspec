1743cde54269e58ceff6e2454059c3c9efcb14fb
Robert Low <robert.low91@gmail.com>
2022-11-22 12:00:33 +0000

    strct double method invoked

---
+++ spec/double_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe 'a random double' do
+  it 'only allows defined methods to be invoked' do
+    stuntman = double("Mr. Danger", fall_off_ladder: 'Ouch',
+      light_on_fire: true)
+
+    expect(stuntman.fall_off_ladder).to eq('Ouch')
+    expect(stuntman.light_on_fire).to eq(true)
+  end
+end
