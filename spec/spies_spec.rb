a4b2df8d24f34b0c3d3d882978e1919ae8a14728
Robert Low <robert.low91@gmail.com>
2022-11-22 17:08:43 +0000

    spies

---
+++ spec/spies_spec.rb
@@ -0,0 +1,24 @@
+RSpec.describe 'spies' do
+  let(:animal) { spy('animal') }
+
+  it 'confirmas that a message has been received' do
+    # the invocation comes before the expect (eat_food)
+    animal.eat_food
+    expect(animal).to have_received(:eat_food)
+    expect(animal).not_to have_received(:eat_human)
+  end
+
+  it 'resets between examples' do
+    expect(animal).not_to have_received(:eat_food)
+  end
+
+  it 'retains the same functionality as a double' do
+    animal.eat_food
+    animal.eat_food
+    animal.eat_food('Sushi')
+    expect(animal).to have_received(:eat_food).exactly(3).times
+    expect(animal).to have_received(:eat_food).at_least(2).times
+    expect(animal).to have_received(:eat_food).with('Sushi')
+    expect(animal).to have_received(:eat_food).once.with('Sushi')
+  end
+end
